44d41bb0a465ae042dbe5cc7a8f89c5aa43de8fb
Apostolos Panagiotopoulos <ap.panagiotopoulos@gmail.com>
2022-12-07 10:40:39 +0100

    Replace my transform with the one from the original paper (temporarily for debugging)

--- pretrain.py
+++ pretrain.py
@@ -9,8 +9,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 from utils import ContrastiveTransformations
 
-
-model_names = ['vit_small', 'vit_base']
+import loader
 
 
 def main(args):
@@ -21,27 +20,64 @@ def main(args):
     if args.seed:
         pl.seed_everything(args.seed)
     
-    # Set-up model
-    print('Building MoCo-v3...')
-    model = MoCo(args.encoder, args.out_dim, args.mlp_dim,
-                 args.tau, args.mu, args.lr, args.weight_decay, args.max_epochs)
-    
     # Set-up training data
     print('Downloading / Loading dataset...')
-    contrast_transforms = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop(size=96),
-        transforms.RandomApply([transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)], p=0.8),
+    # contrast_transforms = transforms.Compose([
+    #     transforms.RandomHorizontalFlip(),
+    #     transforms.RandomResizedCrop(size=96),
+    #     transforms.RandomApply([transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)], p=0.8),
+    #     transforms.RandomGrayscale(p=0.2),
+    #     transforms.GaussianBlur(kernel_size=9),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.5,), (0.5,)),
+    # ])
+    # unlabeled_dataset = datasets.STL10(root=args.data_dir, split='unlabeled', download=True,
+    #                          transform=ContrastiveTransformations(contrast_transforms))
+    augmentation1 = [
+        transforms.RandomResizedCrop(96, scale=(0.08, 1.)),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)  # not strengthened
+        ], p=0.8),
         transforms.RandomGrayscale(p=0.2),
-        transforms.GaussianBlur(kernel_size=9),
+        transforms.RandomApply([loader.GaussianBlur([.1, 2.])], p=1.0),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)),
-    ])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+
+    augmentation2 = [
+        transforms.RandomResizedCrop(96, scale=(0.08, 1.)),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)  # not strengthened
+        ], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomApply([loader.GaussianBlur([.1, 2.])], p=0.1),
+        transforms.RandomApply([loader.Solarize()], p=0.2),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+
     dataset = datasets.STL10(root=args.data_dir, split='unlabeled', download=True,
-                             transform=ContrastiveTransformations(contrast_transforms))
+                             transform=loader.TwoCropsTransform(transforms.Compose(augmentation1), transforms.Compose(augmentation2)))
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True,
                             drop_last=True, pin_memory=True, num_workers=args.num_workers)
+   
     
+    # Set-up model
+    print('Building MoCo-v3...')
+    model = MoCo(
+        args.encoder,
+        args.out_dim,
+        args.mlp_dim,
+        args.tau,
+        args.mu,
+        args.lr,
+        args.weight_decay,
+        args.warmup_epochs * len(dataloader) / num_devices,
+        args.max_epochs * len(dataloader) / num_devices,
+    )
+        
     # Train
     logger = TensorBoardLogger(save_dir='tb_logs', name='pre-train', default_hp_metric=False)
     trainer = pl.Trainer(
@@ -61,10 +97,7 @@ if __name__ == '__main__':
     
     # MoCo-v3 args
     parser.add_argument('--encoder', metavar='ENCODER', default='vit_small',
-                        choices=model_names,
-                        help='encoder architecture: ' +
-                            ' | '.join(model_names) +
-                            ' (default: vit_small)')
+                        help='encoder architecture: (default: vit_small)')
     parser.add_argument('--out-dim', default=256, type=int,
                         help='feature dimension (default: 256)')
     parser.add_argument('--mlp-dim', default=4096, type=int,
@@ -79,22 +112,20 @@ if __name__ == '__main__':
                         help='Root dir to store STL10')
 
     # Trainer args
-    parser.add_argument('--max-epochs', default=100, type=int, metavar='N',
-                        help='number of total epochs to run')
+    parser.add_argument('--max-epochs', default=15, type=int,
+                        help='Total number of training epochs (default: 10)')
+    parser.add_argument('--warmup-epochs', default=5, type=int,
+                        help='Epochs to linearly warm-up the learning rate (default: 1)')
     parser.add_argument('-b', '--batch-size', default=128, type=int,
-                        metavar='N',
-                        help='mini-batch size (default: 1024), this is the total '
-                            'batch size of all GPUs on all nodes when '
-                            'using Data Parallel or Distributed Data Parallel')
+                        metavar='N', help='mini-batch size per GPU (default: 128)')
     parser.add_argument('-j', '--num-workers', default=6, type=int, metavar='N',
                         help='number of data loading workers (default: 6)')
     parser.add_argument('--seed', default=None, type=int,
-                        help='seed for initializing training. ')
+                        help='seed for initializing training.')
     parser.add_argument('--lr', '--learning-rate', default=1.5e-4, type=float,
-                        metavar='LR', help='initial (base) learning rate', dest='lr')
+                            metavar='LR', help='initial (base) learning rate', dest='lr')
     parser.add_argument('--wd', '--weight-decay', default=0.1, type=float,
-                        metavar='W', help='weight decay (default: 0.1)',
-                        dest='weight_decay')
+                        metavar='W', help='weight decay (default: 0.1)', dest='weight_decay')
     
     args = parser.parse_args()
     
